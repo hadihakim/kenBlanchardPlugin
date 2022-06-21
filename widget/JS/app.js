f169fc5a2cc882672031ab26dcd019c31444702e
Hiba-Almade <hibaalmade998@gmail.com>
2022-06-21 21:36:50 +0300

    create template 1 "for you section"

--- widget/JS/app.js
+++ widget/JS/app.js
@@ -2,12 +2,17 @@
 let dummyData = [];
 apiCall().then((res) => {
   dummyData = res;
-  render();
+  render(dummyData);
+  console.log("dddd", dummyData);
 });
 
-const render = () => {
-    const container1 = document.getElementById("container1");
-    const container2 = document.getElementById("container2");
-    render1(dummyData,container1,true);
-    render1(dummyData,container2,false);
+const render = (dummyData) => {
+    // const container1 = document.getElementById("container1");
+    // const container2 = document.getElementById("container2");
+    // render1(dummyData,container1,true);
+    // render1(dummyData,container2,false);
+
+    const forYouContainer = document.getElementById("for-you-container");
+    forYouRender(dummyData,forYouContainer);
+
 };
--- widget/JS/templates/template1.js
+++ widget/JS/templates/template1.js
@@ -1,8 +1,21 @@
-const render1 = (dummyData, container, duration) => {
-  const template1 = document.getElementById("template1");
-  const firstClone = template1.content.cloneNode(true);
-  container.appendChild(firstClone);
-  if (duration) {
-    span.innerText = "9:h";
+const  forYouRender = (dummyData, container, duration) => {
+  const template = document.getElementById("for-you-template");
+  // const firstClone = template.content.cloneNode(true);
+  for(let i=0; i<dummyData.length; i++) {
+    const firstClone = template.content.cloneNode(true);
+ 
+    let header=firstClone.getElementById("for-you-header");
+    header.innerHTML = dummyData[i].title;
+    let note=firstClone.getElementById("for-you-note");
+    note.innerHTML=dummyData[i].description;
+    let image=firstClone.getElementById("for-you-img");
+   image.src=dummyData[i].image;
+   container.appendChild(firstClone);
   }
+  
+
+  // if (duration) {
+  //   span.innerText = "9:h";
+  // }
+
 };
