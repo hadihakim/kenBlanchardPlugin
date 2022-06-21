4957c0183a678f3a36c1728d8190da7a946c5129
AlaaN-Smadi <alaasmadi1010@gmail.com>
2022-06-21 23:06:20 +0300

    fix class names

--- widget/JS/templates/template1.js
+++ widget/JS/templates/template1.js
@@ -1,14 +1,14 @@
 const  forYouRender = (dummyData, container, duration) => {
-  const template = document.getElementById("for-you-template");
+  const template = document.getElementById("template_1");
   // const firstClone = template.content.cloneNode(true);
   for(let i=0; i<dummyData.length; i++) {
     const firstClone = template.content.cloneNode(true);
 
-    let header=firstClone.getElementById("for-you-header");
+    let header=firstClone.getElementById("card-Text-Header");
     header.innerHTML = dummyData[i].title;
-    let note=firstClone.getElementById("for-you-note");
+    let note=firstClone.getElementById("card-Text-Note");
     note.innerHTML=dummyData[i].description;
-    let image=firstClone.getElementById("for-you-data");
+    let image=firstClone.getElementById("card_body");
 
    image.style.backgroundImage= `linear-gradient(0deg, rgba(0, 0, 0, 0.32), rgba(0, 0, 0, 0.32)), url(${dummyData[i].image})`;
    container.appendChild(firstClone);
