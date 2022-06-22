e9b36bfa964799c9d3d12593bc613b63c90a96bc
AlaaN-Smadi <alaasmadi1010@gmail.com>
2022-06-22 17:48:39 +0300

    search ba functionalities

---
+++ widget/JS/helper/searchBar.js
@@ -0,0 +1,39 @@
+'use strict';
+
+function testDrawer() {
+	buildfire.components.drawer.open(
+		{
+			content: 'Topics',
+			multiSelection: true,
+			height: '100%',
+			allowSelectAll: true,
+			multiSelectionActionButton: { text: 'Apply', type: 'success' },
+			enableFilter: true,
+			isHTML: false,
+			triggerCallbackOnUIDismiss: false,
+			autoUseImageCdn: true,
+			listItems: [
+				{ text: 'Coaching', selected: false },
+				{ text: 'Conflict', selected: false },
+				{ text: 'Customer Service', selected: false },
+				{ text: 'Change & Innovation', selected: false },
+				{ text: 'Diversity, Equity & Inclusion ', selected: false },
+				{ text: 'Change & Innovation', selected: false },
+				{ text: 'Leading People', selected: false },
+				{ text: 'Performance Management', selected: false },
+				{ text: 'Personal Effectiveness', selected: false },
+				{ text: 'Team Effectiveness', selected: false },
+				{ text: 'Trust', selected: false },
+				{ text: 'Working with Others', selected: false }
+			]
+		},
+		(err, result) => {
+			if (err) return console.error(err);
+			console.log("Selected Contacts", result);
+		}
+
+	);
+}
+
+let filterIcon = document.getElementById("filterIcon");
+filterIcon.addEventListener("click", testDrawer);
