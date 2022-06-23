aa706ebf6f52842b5fbea1f8cd91dfbecf8243b7
ibrahemomari <ibrahem.omari96@gmail.com>
2022-06-23 22:15:47 +0300

    crop images

--- widget/js/templates/template.js
+++ widget/js/templates/template.js
@@ -4,16 +4,18 @@ const templates = () => {
     let data = apiData.data.assets_info[assetsId];
 
     const template = document.getElementById("forYouTemplate");
-   
 
-    for (let i = 0; i <6; i++) {
+    for (let i = 0; i < 6; i++) {
       const firstClone = template.content.cloneNode(true);
       let title = firstClone.querySelectorAll(".card-Text-Header");
       let note = firstClone.querySelectorAll(".card-Text-Note");
       let image = firstClone.getElementById("card_body");
       title[0].innerHTML = data.meta.title;
-      note[0].innerHTML =data.meta.description;
-      image.style.backgroundImage = `linear-gradient(0deg, rgba(0, 0, 0, 0.32), rgba(0, 0, 0, 0.32)), url('${data.meta.image}')`;
+      note[0].innerHTML = data.meta.description;
+      image.style.backgroundImage = `linear-gradient(0deg, rgba(0, 0, 0, 0.32), rgba(0, 0, 0, 0.32)), url('${buildfire.imageLib.cropImage(
+        data.meta.image,
+        { size: "full_width", aspect: "16:9" }
+      )}')`;
       container.appendChild(firstClone);
     }
   };
@@ -29,7 +31,10 @@ const templates = () => {
       let category = nodesClone.querySelectorAll(".category");
       let title = nodesClone.querySelectorAll(".title");
       let duration = nodesClone.querySelectorAll(".duration");
-      image[0].style.backgroundImage = `url('${data.meta.image}')`;
+      image[0].style.backgroundImage = `url('${buildfire.imageLib.cropImage(
+        data.meta.image,
+        { size: "full_width", aspect: "16:9" }
+      )}')`;
       category[0].innerText = data.type;
       title[0].innerText = data.meta.title;
       if (durationState) {
@@ -52,7 +57,10 @@ const templates = () => {
       let title = nodesClone.querySelectorAll(".title");
       let duration = nodesClone.querySelectorAll(".duration");
       let description = nodesClone.querySelectorAll(".description");
-      image[0].style.backgroundImage = `url('${data.meta.image}')`;
+      image[0].style.backgroundImage = `url('${buildfire.imageLib.cropImage(
+        data.meta.image,
+        { size: "full_width", aspect: "4:3" }
+      )}')`;
       title[0].innerText = data.meta.title;
       description[0].innerText = data.meta.description;
       if (durationState) {
@@ -67,13 +75,18 @@ const templates = () => {
   const trendingRender = (data, containerId) => {
     const container = document.getElementById(containerId);
     const trendingTemplate = document.getElementById("trendingTemplate");
-   for(let i = 0; i < 6 ; i++){
+    for (let i = 0; i < 6; i++) {
       const nodesClone = trendingTemplate.content.cloneNode(true);
       let title = nodesClone.querySelectorAll(".trending-span");
       title[0].innerHTML = "trending";
       container.appendChild(nodesClone);
-    };
+    }
   };
 
-  return { forYouRender, recommendedCardRender, seeAllCardsRender, trendingRender };
+  return {
+    forYouRender,
+    recommendedCardRender,
+    seeAllCardsRender,
+    trendingRender,
+  };
 };
