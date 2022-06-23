adc9579bd697a1f26bfb1f141a2a5b9b87823bbf
AlaaN-Smadi <alaasmadi1010@gmail.com>
2022-06-23 23:54:50 +0300

    add apptheme color handlers

--- widget/js/app.js
+++ widget/js/app.js
@@ -1,85 +1,89 @@
 "use strict";
 
+const { getAppTheme, setAppTheme } = utilities();
+
 const {
-  forYouRender,
-  recommendedCardRender,
-  seeAllCardsRender,
-  trendingRender,
+	forYouRender,
+	recommendedCardRender,
+	seeAllCardsRender,
+	trendingRender,
 } = templates();
 const seeAllBtnAction = () => {
-  let mainContainer = document.getElementById("mainPage");
-  let seeAllContainer = document.getElementById("seeAllContainer");
-  if (!mainContainer.classList.contains("hidden")) {
-    buildfire.history.push("Personal Home Page from See All");
-    mainContainer.classList.add("hidden");
-    userContainer.classList.add("hidden");
-    sortIcon.classList.remove("hidden");
-  } else if (!subPage.classList.contains("hidden")) {
-    buildfire.history.push("Explore page");
-    subPage.classList.add("hidden");
-  }
-  seeAllContainer.classList.remove("hidden");
-  scrollTop();
+	let mainContainer = document.getElementById("mainPage");
+	let seeAllContainer = document.getElementById("seeAllContainer");
+	if (!mainContainer.classList.contains("hidden")) {
+		buildfire.history.push("Personal Home Page from See All");
+		mainContainer.classList.add("hidden");
+		userContainer.classList.add("hidden");
+		sortIcon.classList.remove("hidden");
+	} else if (!subPage.classList.contains("hidden")) {
+		buildfire.history.push("Explore page");
+		subPage.classList.add("hidden");
+	}
+	seeAllContainer.classList.remove("hidden");
+	scrollTop();
 };
 
 const cardRender = (sectionId, data) => {
-  const sectionsContainer = document.getElementById(sectionId);
-  data.forEach((element) => {
-    if (element.id === "explore") {
-      const container = document.getElementById(element.containerId);
-      seeAllCardsRender(fakeData, container, element.duration);
-    } else if (element.id === "for-you-section") {
-      let sectionInnerHTML = `
-			<p class="sectionTitle">${element.title}</p>
+	const sectionsContainer = document.getElementById(sectionId);
+	data.forEach((element) => {
+		if (element.id === "explore") {
+			const container = document.getElementById(element.containerId);
+			seeAllCardsRender(fakeData, container, element.duration);
+		} else if (element.id === "for-you-section") {
+			let sectionInnerHTML = `
+			<p class="sectionTitle headerText-AppTheme">${element.title}</p>
 			<div id="${element.containerId}"></div>
   `;
-      ui.createElement(
-        "section",
-        sectionsContainer,
-        sectionInnerHTML,
-        element.className,
-        element.id
-      );
-      const container = document.getElementById(element.containerId);
-      forYouRender(fakeData, container, element.duration);
-    } else {
-      let sectionInnerHTML = `
+			ui.createElement(
+				"section",
+				sectionsContainer,
+				sectionInnerHTML,
+				element.className,
+				element.id
+			);
+			const container = document.getElementById(element.containerId);
+			forYouRender(fakeData, container, element.duration);
+		} else {
+			let sectionInnerHTML = `
       <div class="container-header">
-          <p class="title">${element.title}</p>
-          <span class="seeAll-btn" onclick="seeAllBtnAction('${element.id}')">${element.seeAllBtn}</span>
+          <p class="title headerText-AppTheme">${element.title}</p>
+          <span class="seeAll-btn info-link-AppTheme" onclick="seeAllBtnAction('${element.id}')">${element.seeAllBtn}</span>
       </div>
           <div id="${element.containerId}" class="${element.containerClassName}">
       </div>
   `;
 
-      ui.createElement(
-        "section",
-        sectionsContainer,
-        sectionInnerHTML,
-        element.className,
-        element.id
-      );
+			ui.createElement(
+				"section",
+				sectionsContainer,
+				sectionInnerHTML,
+				element.className,
+				element.id
+			);
 
-      const container = document.getElementById(element.containerId);
-      recommendedCardRender(fakeData, container, element.duration);
-    }
-  });
+			const container = document.getElementById(element.containerId);
+			recommendedCardRender(fakeData, container, element.duration);
+		}
+	});
 
-  const exploreBtn = document.getElementById("exploreButton");
-  exploreBtn.addEventListener("click", () => {
-    mainPage.classList.add("hidden");
-    subPage.classList.remove("hidden");
-    userContainer.classList.add("hidden");
-    sortIcon.classList.remove("hidden");
-    buildfire.history.push("Personal Home Page");
-    scrollTop();
-  });
+	const exploreBtn = document.getElementById("exploreButton");
+	exploreBtn.addEventListener("click", () => {
+		mainPage.classList.add("hidden");
+		subPage.classList.remove("hidden");
+		userContainer.classList.add("hidden");
+		sortIcon.classList.remove("hidden");
+		buildfire.history.push("Personal Home Page");
+		scrollTop();
+	});
 };
 
 const init = () => {
-  cardRender("sectionsContainer", config.sectionConfig);
-  cardRender("exploreContainer", config.exploreConfig);
-  trendingRender(fakeData, "trendingContainer");
-};
+	getAppTheme();
+	cardRender("sectionsContainer", config.sectionConfig);
+	cardRender("exploreContainer", config.exploreConfig);
+	trendingRender(fakeData, "trendingContainer");
+	setAppTheme();
+}
 
 init();
--- widget/js/utilities/utilities.js
+++ widget/js/utilities/utilities.js
@@ -9,15 +9,73 @@ const utilities = () => {
 		return cropedImage;
 	}
 
-	const timeConvert=(n)=> {
+	const timeConvert = (n) => {
 		let num = n;
 		let hours = (num / 60);
 		let rHours = Math.floor(hours);
 		let minutes = (hours - rHours) * 60;
 		let rMinutes = Math.round(minutes);
-		return  rHours + "h " + rMinutes + "min";
-	  }
+		return rHours + "h " + rMinutes + "min";
+	}
 
-	  return {cropImage, timeConvert}
+	const getAppTheme = () => {
+		buildfire.appearance.getAppTheme((err, appTheme) => {
+			if (err) return console.error(err);
+			config.appTheme = appTheme;
+		});
+	}
 
+	const setThemeHandler = (arr, type, color) => {
+		for (let i = 0; i < arr.length; i++) {
+			switch (type) {
+				case "color":
+					arr[i].style.color = color;
+					break;
+				case "back":
+					arr[i].style.backgroundColor = color;
+					break;
+				default:
+					break;
+			}
+		}
+	}
+
+	const setAppTheme = () => {
+		let colorCollections = [
+			{
+				elements: document.getElementsByClassName("icon"),
+				colorType: "color",
+				colorDegree: config.appTheme.colors.icons
+			}, {
+				elements: document.getElementsByClassName("headerText-AppTheme"),
+				colorType: "color",
+				colorDegree: config.appTheme.colors.headerText
+			}, {
+				elements: document.getElementsByClassName("barText-AppTheme"),
+				colorType: "color",
+				colorDegree: config.appTheme.colors.titleBarTextAndIcons
+			}, {
+				elements: document.getElementsByClassName("bodyText-AppTheme"),
+				colorType: "color",
+				colorDegree: config.appTheme.colors.bodyText
+			}, {
+				elements: document.getElementsByClassName("userContainer"),
+				colorType: "back",
+				colorDegree: config.appTheme.colors.primaryTheme
+			}, {
+				elements: document.getElementsByClassName("info-btn-AppTheme"),
+				colorType: "back",
+				colorDegree: config.appTheme.colors.infoTheme
+			}, {
+				elements: document.getElementsByClassName("info-link-AppTheme"),
+				colorType: "color",
+				colorDegree: config.appTheme.colors.infoTheme
+			}
+		];
+
+		colorCollections.forEach(element => {
+			setThemeHandler(element.elements, element.colorType, element.colorDegree);
+		})
+	}
+	return { cropImage, timeConvert, getAppTheme, setAppTheme }
 }
