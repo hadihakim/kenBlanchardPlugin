54074c4fbab163b968908990d23fc725d3107050
ibrahemomari <ibrahem.omari96@gmail.com>
2022-06-28 19:33:11 +0300

    add user config

--- widget/js/app.js
+++ widget/js/app.js
@@ -2,55 +2,50 @@
 
 const { getAppTheme, setAppTheme, initBack, scrollFcn } = utilities();
 
-const {
-	filterAndPrintData,
-	seeAllCardsRender,
-	trendingRender,
-} = templates();
+const { filterAndPrintData, seeAllCardsRender, trendingRender } = templates();
 
 // control variables
 let currentPage = 1;
 const limit = 10;
 let total = 0;
 
-
 const seeAllBtnAction = (title) => {
-	mainContainer.addEventListener('scroll', scrollFcn);
+  mainContainer.addEventListener("scroll", scrollFcn);
 
-	scrollTop();
-	document.getElementById("seeAllContainer").innerHTML = "";
-	config.activeSeeAll = title;
-	let mainPage = document.getElementById("mainPage");
-	let seeAllContainer = document.getElementById("seeAllContainer");
-	if (!mainPage.classList.contains("hidden")) {
-		buildfire.history.push("Personal Home Page from See All");
-		mainPage.classList.add("hidden");
-		userContainer.classList.add("hidden");
-		sortIcon.classList.remove("hidden");
-	} else if (!subPage.classList.contains("hidden")) {
-		buildfire.history.push("Explore page");
-		subPage.classList.add("hidden");
-	}
-	seeAllCardsRender(
-		fakeData,
-		document.getElementById("seeAllContainer"),
-		true,
-		() => { }
-	);
-	seeAllContainer.classList.remove("hidden");
-	config.isSeeAllScreen = true;
+  scrollTop();
+  document.getElementById("seeAllContainer").innerHTML = "";
+  config.activeSeeAll = title;
+  let mainPage = document.getElementById("mainPage");
+  let seeAllContainer = document.getElementById("seeAllContainer");
+  if (!mainPage.classList.contains("hidden")) {
+    buildfire.history.push("Personal Home Page from See All");
+    mainPage.classList.add("hidden");
+    userContainer.classList.add("hidden");
+    sortIcon.classList.remove("hidden");
+  } else if (!subPage.classList.contains("hidden")) {
+    buildfire.history.push("Explore page");
+    subPage.classList.add("hidden");
+  }
+  seeAllCardsRender(
+    fakeData,
+    document.getElementById("seeAllContainer"),
+    true,
+    () => {}
+  );
+  seeAllContainer.classList.remove("hidden");
+  config.isSeeAllScreen = true;
 };
 
 const cardRender = (sectionId, data) => {
-	const sectionsContainer = document.getElementById(sectionId);
-	data.forEach((element) => {
-		if (element.id === "explore") {
-			const container = document.getElementById(element.containerId);
-			seeAllCardsRender(fakeData, container, element.duration, () => { });
-		} else {
-			let sectionInnerHTML;
-			if (element.title != "Just for you") {
-				sectionInnerHTML = `
+  const sectionsContainer = document.getElementById(sectionId);
+  data.forEach((element) => {
+    if (element.id === "explore") {
+      const container = document.getElementById(element.containerId);
+      seeAllCardsRender(fakeData, container, element.duration, () => {});
+    } else {
+      let sectionInnerHTML;
+      if (element.title != "Just for you") {
+        sectionInnerHTML = `
 				<div class="container-header">
 					<p class="title headerText-AppTheme">${element.title}</p>
 					<span class="seeAll-btn info-link-AppTheme" onclick="seeAllBtnAction('${element.title}')">${element.seeAllBtn}</span>
@@ -58,46 +53,74 @@ const cardRender = (sectionId, data) => {
 					<div id="${element.containerId}" class="${element.containerClassName}">
 				</div>
 				  `;
-			} else {
-				sectionInnerHTML = `
+      } else {
+        sectionInnerHTML = `
 				<p class="sectionTitle headerText-AppTheme">${element.title}</p>
 					<div id="${element.containerId}"></div>
 				`;
-			}
-			ui.createElement(
-				"section",
-				sectionsContainer,
-				sectionInnerHTML,
-				element.className,
-				element.id
-			);
-			const container = document.getElementById(element.containerId);
-			filterAndPrintData(fakeData, container, element.duration, element.title, element.id);
-		}
-	});
+      }
+      ui.createElement(
+        "section",
+        sectionsContainer,
+        sectionInnerHTML,
+        element.className,
+        element.id
+      );
+      const container = document.getElementById(element.containerId);
+      filterAndPrintData(
+        fakeData,
+        container,
+        element.duration,
+        element.title,
+        element.id
+      );
+    }
+  });
 
-	const exploreBtn = document.getElementById("exploreButton");
-	exploreBtn.addEventListener("click", () => {
-		mainPage.classList.add("hidden");
-		subPage.classList.remove("hidden");
-		userContainer.classList.add("hidden");
-		sortIcon.classList.remove("hidden");
-		buildfire.history.push("Personal Home Page");
-		scrollTop();
-	});
+  const exploreBtn = document.getElementById("exploreButton");
+  exploreBtn.addEventListener("click", () => {
+    mainPage.classList.add("hidden");
+    subPage.classList.remove("hidden");
+    userContainer.classList.add("hidden");
+    sortIcon.classList.remove("hidden");
+    buildfire.history.push("Personal Home Page");
+    scrollTop();
+  });
 };
 
-function open(id){
-console.log(id, "iffffffffffffff");
+function open(id) {
+  console.log(id, "iffffffffffffff");
 }
 
+const getUser = (data) => {
+  let userName = document.getElementById("userName");
+  let userProfilePicture = document.getElementById("userProfilePicture");
+  let userAchievementIcon = document.getElementById("userAchievementIcon");
+  let growthProfile = document.getElementById("growthProfile");
+  if (data.isLoggedIn) {
+    let userAchievements = data.badges.filter((el) => el.active === true);
+    userName.innerText = data.firstName + " " + data.lastName;
+    growthProfile.innerText = data.growthProfile;
+    userProfilePicture.src = data.profilePicture;
+    userProfilePicture.alt = data.firstName;
+    userAchievementIcon.src = userAchievements[0].achievementIcon;
+    userAchievementIcon.alt = userAchievements[0].achievementTitle;
+    config.filterArr = data.recommendedTags;
+  } else {
+    userProfilePicture.src = "../../../../styles/media/avatar-placeholder.png";
+    userName.innerText = "Anonymous";
+    growthProfile.innerText = "Profile Growth";
+    userAchievementIcon.src = "../../../../styles/media/holder-1x1.png";
+  }
+};
 const init = () => {
-	getAppTheme();
-	cardRender("sectionsContainer", config.sectionConfig);
-	cardRender("exploreContainer", config.exploreConfig);
-	trendingRender(fakeData, "trendingContainer");
-	setAppTheme();
-	initBack();
-}
+  getUser(config.userConfig);
+  getAppTheme();
+  cardRender("sectionsContainer", config.sectionConfig);
+  cardRender("exploreContainer", config.exploreConfig);
+  trendingRender(fakeData, "trendingContainer");
+  setAppTheme();
+  initBack();
+};
 
 init();
--- widget/js/helper/uiConfig.js
+++ widget/js/helper/uiConfig.js
@@ -127,16 +127,40 @@ const config = {
       duration: false,
     },
   ],
+  userConfig: {
+    firstName: "Michel",
+    lastName: "Scoot",
+    email: "michel@gmail.com",
+    growthProfile:"Newcomer",
+    profilePicture:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWHkQVsb4b-CS96W5FbUgyBLTrtpBNE-DSHg&usqp=CAU",
+    badges: [
+      {
+        active:false,
+        achievementTitle: "achievement",
+        achievementIcon:
+          "https://github.githubassets.com/images/modules/profile/achievements/pull-shark-default.png",
+      },
+      {
+        active:true,
+        achievementTitle: "achievement 2",
+        achievementIcon:
+          "https://github.githubassets.com/images/modules/profile/achievements/yolo-default.png",
+      },
+    ],
+    recommendedTags: ["Conflict", "Coaching"],
+    isLoggedIn: true,
+  },
   appTheme: {},
   filterArr: [],
   sortType: "Default",
   activeSeeAll: "",
-  cardsLimit: 1,
+  cardsLimit: 7,
   isSeeAllScreen: false,
   fetchingNextPage: false,
   page: 1,
   pageSize: 5,
   lastIndex: 0,
   renderedCard: 0,
-  search:""
+  search: "",
 };
